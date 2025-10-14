201e58fc7aa7d3cada1a26d81837dfff380c13be
bonie-tak <kubbik.szymon@gmail.com>
2025-10-14 13:57:02 +0200

    Create kubica_zewnętrzny.js

---
+++ kubica_zewnętrzny.js
@@ -0,0 +1,4 @@
+document.write('<span style="color:maroon; font-size:30px; font-weight:bold; text-decoration:line-through;">ostatnia modyfikacja strony</span><br>');
+
+
+document.write('<span style="color:red; font-size:20px; text-decoration:line-through;">' + document.lastModified + '</span>');
